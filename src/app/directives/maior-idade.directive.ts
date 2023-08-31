d2c03f91f9d329e8103ea7919be373cd91a7d338
codi.desenvolvimento <desenvolvimento4@codi.com.br>
2023-08-31 13:41:44 -0300

    Adicionada validação de idade

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,25 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALUE_ACCESSOR, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: '[maiorIdadeValidator]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: MaiorIdadeDirective,
+    multi: true
+  }]
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validator{
+
 
   constructor() { }
+
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoAtual = new Date().getFullYear();
+    const ehMaior = (anoNascimento +18) <= anoAtual;
+    return ehMaior ? null : {'maiorIdadeValidator': true}
+  }
 
 }
